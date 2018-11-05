ca5fc2ddd48452f2eb232f956ae1bbe32820e2bd
Domagoj Vlaho <domagoj.vlaho@gmail.com>
2018-11-05 19:02:18 +0100

    Initial commit - Add the whole project to git commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+//baguetteBox.run('.gallery');
+
+//$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+/*                event.preventDefault();
+                $(this).ekkoLightbox();
+            });*/
+
+$("#button1").click(function(){
+   $(this).toggleClass("rotate-180");
+})
+
+function openModal() {
+		if(event.target.id==="view1")
+		{
+				$('#lightboxImage1')[0].click();
+		} else if(event.target.id==="view2"){
+				$('#lightboxImage2')[0].click();
+		} else if(event.target.id==="view3"){
+				$('#lightboxImage3')[0].click();
+		} else if(event.target.id==="view4"){
+				$('#lightboxImage4')[0].click();
+		} else if(event.target.id==="view5"){
+				$('#lightboxImage5')[0].click();
+		} else if(event.target.id==="view6"){
+				$('#lightboxImage6')[0].click();
+		}
+
+            
+        }
+
+let clicks1 = 0;
+let clicks2 = 0;
+let clicks3 = 0;
+let clicks4 = 0;
+let clicks5 = 0;
+let clicks6 = 0;
+
+function onClickB1() {
+    clicks1 += 1;
+    document.getElementById("clicks1").innerHTML = clicks1;
+};
+
+function onClickB2() {
+    clicks2 += 1;
+    document.getElementById("clicks2").innerHTML = clicks2;
+};
+
+function onClickB3() {
+    clicks3 += 1;
+    document.getElementById("clicks3").innerHTML = clicks3;
+};
+
+function onClickB4() {
+    clicks4 += 1;
+    document.getElementById("clicks4").innerHTML = clicks4;
+};
+
+function onClickB5() {
+    clicks5 += 1;
+    document.getElementById("clicks5").innerHTML = clicks5;
+};
+
+function onClickB6() {
+    clicks6 += 1;
+    document.getElementById("clicks6").innerHTML = clicks6;
+};
