728c16d08c0e0a29556b6ef36f5d453de6255d10
Domagoj Vlaho <domagoj.vlaho@gmail.com>
2018-11-08 02:06:52 +0100

    Removed Bootstrap and done the navbar, element allignment and gallery grid with basic CSS

--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@
                 $(this).ekkoLightbox();
             });*/
 
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
 $("#button1").click(function(){
    $(this).toggleClass("rotate-180");
 })
